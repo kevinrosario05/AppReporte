f6f94ec17085e217b89ceed35cd17f2456b2e752
kevinrosario05 <kevinrosario05@gmail.com>
2020-12-18 03:28:53 +0100

    subiendo proyecto

---
+++ Parcial1 P3/Migrations/20201006211304_InitialCreate.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parcial1_P3.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "accidentes",
+                columns: table => new
+                {
+                    accidenteId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    placa = table.Column<string>(nullable: true),
+                    marca = table.Column<string>(nullable: true),
+                    modelo = table.Column<string>(nullable: true),
+                    color = table.Column<string>(nullable: true),
+                    descripcion = table.Column<string>(nullable: true),
+                    cedula = table.Column<string>(nullable: true),
+                    nombre = table.Column<string>(nullable: true),
+                    apellido = table.Column<string>(nullable: true),
+                    accidenteId1 = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_accidentes", x => x.accidenteId);
+                    table.ForeignKey(
+                        name: "FK_accidentes_accidentes_accidenteId1",
+                        column: x => x.accidenteId1,
+                        principalTable: "accidentes",
+                        principalColumn: "accidenteId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    BlogId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.BlogId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: true),
+                    Content = table.Column<string>(nullable: true),
+                    BlogId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Posts_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blogs",
+                        principalColumn: "BlogId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_accidentes_accidenteId1",
+                table: "accidentes",
+                column: "accidenteId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_BlogId",
+                table: "Posts",
+                column: "BlogId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "accidentes");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
---
+++ Parcial1 P3/model.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Parcial1_P3
+{
+   
+
+    
+ 
+        public class Datos : DbContext
+        {
+            public DbSet<Blog> Blogs { get; set; }
+            public DbSet<Post> Posts { get; set; }
+        public DbSet<accidente> accidentes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+                => options.UseSqlite("Data Source=Reporte.db");
+        }
+
+        public class Blog
+        {
+            public int BlogId { get; set; }
+            public string Url { get; set; }
+
+            public List<Post> Posts { get; } = new List<Post>();
+        }
+
+        public class Post
+        {
+            public int PostId { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+
+            public int BlogId { get; set; }
+            public Blog Blog { get; set; }
+        }
+    public class accidente
+    {
+        public int accidenteId { get; set; }
+        public string placa { get; set; }
+        public string marca { get; set; }
+        public string modelo { get; set; }
+        public string color { get; set; }
+        public string descripcion { get; set; }
+
+        public string cedula { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public List<accidente> accidentes { get; } = new List<accidente>();
+
+
+    }
+}
+
